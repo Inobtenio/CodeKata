a83d8d8563ba2b039fac8203c9175c67c93998b9
Kevin Martin <knmartinm@gmail.com>
2020-02-04 08:55:49 -0500

    Test with Sets instead of Arrays

--- Transitive Dependencies/dependencies.rb
+++ Transitive Dependencies/dependencies.rb
@@ -1,3 +1,5 @@
+require 'set'
+
 class Dependencies
 	def initialize
 		@raw_dependencies = Hash.new
@@ -9,32 +11,32 @@ class Dependencies
 	end
 
 	def dependencies_for target
-		@processed_dependencies[target]
+		@processed_dependencies[target].to_a
 	end
 
 	def calculate_dependencies
 		@raw_dependencies.keys.each do |key|
-			@processed_dependencies[key] = all_dependencies_for(@raw_dependencies[key], [key])
+			@processed_dependencies[key] = all_dependencies_for(@raw_dependencies[key], Set[key])
 		end
 	end
 
 	private
 
 	def all_dependencies_for targets, seen_targets
-		dependencies = Array(targets)
+		dependencies = SortedSet.new(targets)
 		targets.each do |target|
 			next if seen_targets.include?(target)
 			dependencies += look_up_or_calculate_dependencies_for(target, seen_targets)
 		end
-		refine(dependencies, seen_targets.first)
+		remove_first_target_from(dependencies, seen_targets)
 	end
 
 	def look_up_or_calculate_dependencies_for target, seen_targets
-		dependencies_for(target) || calculate_dependencies_for(target, seen_targets.push(target))
+		@processed_dependencies[target] || calculate_dependencies_for(target, seen_targets.add(target))
 	end
 
-	def refine array, target_to_exclude
-		array.uniq.sort - [target_to_exclude]
+	def remove_first_target_from set, targets
+		set.delete(targets.first)
 	end
 
 	def calculate_dependencies_for target, seen_targets
